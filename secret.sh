4fb40c818df02dd1c75855137847b95555b82503
Max Jansen <mail@maxjansen.net>
2026-04-17 11:59:39 +0200

    Added K8s job

---
+++ secret.sh
@@ -0,0 +1,5 @@
+kubectl create secret docker-registry registry \
+  --docker-server=laptop-fedora.tail37fe80.ts.net \
+  --docker-username=admin \
+  --docker-password=admin \
+  --docker-email=max.jansen3@student.hu.nl
